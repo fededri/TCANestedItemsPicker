153216dfdae12cc4fc2fab3b1a6bb58cfead5849
Federico Torres <federico.torres@autodesk.com>
2025-05-22 23:13:15 -0600

    update to latest TCA version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["TCANestedItemsPicker"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.18.0")
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.19.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
